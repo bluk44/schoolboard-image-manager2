0d393aa27b56fff10ada3edaba509fc858f849a9
Lukasz Budkowski <bluk44@gmail.com>
2016-01-11 17:22:32 +0100

    wersja testowa metody tworzacej nowy region tablicy

--- src/imagemanager/model/SourceImage.java
+++ src/imagemanager/model/SourceImage.java
@@ -1,9 +1,12 @@
 package imagemanager.model;
 
+import ij.process.ImageProcessor;
 import imageprocessing.Util;
 
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,33 +22,41 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
 
+import test.Test;
+import mpicbg.ij.InverseTransformMapping;
+import mpicbg.models.HomographyModel2D;
+import mpicbg.models.IllDefinedDataPointsException;
+import mpicbg.models.NotEnoughDataPointsException;
+import mpicbg.models.PointMatch;
+
 @Entity
-@Table(name="SOURCEIMAGE", uniqueConstraints={@UniqueConstraint(columnNames={"NAME"})})
-public class SourceImage{
+@Table(name = "SOURCEIMAGE", uniqueConstraints = { @UniqueConstraint(columnNames = { "NAME" }) })
+public class SourceImage {
 	@Transient
 	static float ICON_SCALE = 0.1f;
-	
+
 	@Id
-	@GeneratedValue	
+	@GeneratedValue
 	private Long id;
 	private String name;
 	private Long date;
-	
+
 	@Lob
-	@Basic(fetch=FetchType.EAGER)
+	@Basic(fetch = FetchType.EAGER)
 	private byte[] icon;
 	@Lob
-	@Basic(fetch=FetchType.LAZY)
+	@Basic(fetch = FetchType.LAZY)
 	private byte[] pixels;
-	
-	@ManyToMany(mappedBy="images")
+
+	@ManyToMany(mappedBy = "images")
 	Set<Category> categories = new HashSet<Category>();
-	
-	@OneToMany(mappedBy="sourceImage")
+
+	@OneToMany(mappedBy = "sourceImage")
 	Set<BoardRegion> boardImages = new HashSet<BoardRegion>();
-	
-	public SourceImage(){};
-	
+
+	public SourceImage() {
+	};
+
 	public SourceImage(File file) {
 		String[] tab = file.getName().split("/");
 		this.name = tab[tab.length - 1];
@@ -54,9 +65,9 @@ public class SourceImage{
 		BufferedImage icon = Util.resize(pixels, ICON_SCALE);
 		this.pixels = Util.getByteArray(pixels);
 		this.icon = Util.getByteArray(icon);
-		
+
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -104,7 +115,7 @@ public class SourceImage{
 	public void setCategories(Set<Category> categories) {
 		this.categories = categories;
 	}
-	
+
 	public Set<BoardRegion> getBoardImages() {
 		return boardImages;
 	}
@@ -112,46 +123,100 @@ public class SourceImage{
 	public void setBoardImages(Set<BoardRegion> boardImages) {
 		this.boardImages = boardImages;
 	}
+
+	public void extractBoardRegion(Point[] quadrangle) {
+		BufferedImage image = Util.getBufferedImage(this.pixels);
+
+		ImageProcessor ip1 = Util.convertToImageProcessor(image);
+		ImageProcessor ip2 = ip1.duplicate();
+
+		int x1 = (quadrangle[0].x + quadrangle[3].x) / 2;
+		int y1 = (quadrangle[0].y + quadrangle[1].y) / 2;
+		int x2 = (quadrangle[1].x + quadrangle[2].x) / 2;
+		int y2 = (quadrangle[2].y + quadrangle[3].y) / 2;
+
+		Point[] fixed = new Point[4];
+
+		fixed[0] = new Point(x1, y1);
+		fixed[1] = new Point(x2, y1);
+		fixed[2] = new Point(x2, y2);
+		fixed[3] = new Point(x1, y2);
+
+		ArrayList<PointMatch> m = new ArrayList<PointMatch>();
+
+		// for (int i = 0; i < 4; i++) {
+		// m.add(new PointMatch(new Point(new float[] { distorted.get(i).x,
+		// distorted.get(i).y }), new Point(new float[] {
+		// fixed.get(i).x, fixed.get(i).y })));
+		//
+		// }
+		for (int i = 0; i < 4; i++) {
+			m.add(new PointMatch(new mpicbg.models.Point(new float[] {
+					quadrangle[i].x, quadrangle[i].y }),
+					new mpicbg.models.Point(new float[] { fixed[i].x,
+							fixed[i].y })));
+		}
 		
-//	public BoardRegion extractBoardRegion(Polygon quadrangle){
-//		
-//		// extract board region from source image
-//		
-//		double x1 = (quadrangle.getPoint(0).x + quadrangle.getPoint(3).x) / 2;
-//		double x2 = (quadrangle.getPoint(1).x + quadrangle.getPoint(2).x) / 2;
-//
-//		double y1 = (quadrangle.getPoint(0).y + quadrangle.getPoint(1).y) / 2;
-//		double y2 = (quadrangle.getPoint(2).y + quadrangle.getPoint(3).y) / 2;
-//		
-//		Polygon fixed = new Polygon(new Point(x1, y1), new Point(x2, y1), new Point(x2, y2), new Point(x1, y2));
-//		
-//		BufferedImage pixels = Util.getBufferedImage(this.pixels);
-//     	BufferedImage trans = QuadrangleTransformation.transform(pixels, quadrangle, fixed);
-//		BufferedImage bRegion = Util.subImage(trans, (int) fixed.getPoint(0).x,
-//				(int) fixed.getPoint(0).y, (int) fixed.getPoint(2).x,
-//				(int) fixed.getPoint(2).y);
-//		Color[] colors = new Color[2];
-//		
-//		BufferedImage binarized = BackgroundCleaner.run(bRegion, colors);
-//		
-//		// create new board image
-//		
-//		BoardRegion bimg = new BoardRegion();
-//		
-//		bimg.setIcon(Util.resize(binarized, ICON_SCALE));
-//		bimg.setPixels(binarized);
-//		bimg.setSource(this);
-//		bimg.setSrcQuadrangle(quadrangle);
-//		
-//		if(colors[1] == Color.BLACK){
-//			bimg.setType(BoardRegion.BoardType.BLACK);
-//		} else if(colors[2] == Color.WHITE){
-//			bimg.setType(BoardRegion.BoardType.WHITE);
-//		}
-//				
-//		return bimg;
-//	}
-	
+		HomographyModel2D model = new HomographyModel2D();
+		InverseTransformMapping<HomographyModel2D> mapping = new InverseTransformMapping<HomographyModel2D>(
+				model);
+		
+		try {
+			model.fit(m);
+			mapping.mapInterpolated(ip1, ip2);
+			Test.showImage(ip2.getBufferedImage(), "ip2");
+
+		} catch (NotEnoughDataPointsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllDefinedDataPointsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	// public BoardRegion extractBoardRegion(Polygon quadrangle){
+	//
+	// // extract board region from source image
+	//
+	// double x1 = (quadrangle.getPoint(0).x + quadrangle.getPoint(3).x) / 2;
+	// double x2 = (quadrangle.getPoint(1).x + quadrangle.getPoint(2).x) / 2;
+	//
+	// double y1 = (quadrangle.getPoint(0).y + quadrangle.getPoint(1).y) / 2;
+	// double y2 = (quadrangle.getPoint(2).y + quadrangle.getPoint(3).y) / 2;
+	//
+	// Polygon fixed = new Polygon(new Point(x1, y1), new Point(x2, y1), new
+	// Point(x2, y2), new Point(x1, y2));
+	//
+	// BufferedImage pixels = Util.getBufferedImage(this.pixels);
+	// BufferedImage trans = QuadrangleTransformation.transform(pixels,
+	// quadrangle, fixed);
+	// BufferedImage bRegion = Util.subImage(trans, (int) fixed.getPoint(0).x,
+	// (int) fixed.getPoint(0).y, (int) fixed.getPoint(2).x,
+	// (int) fixed.getPoint(2).y);
+	// Color[] colors = new Color[2];
+	//
+	// BufferedImage binarized = BackgroundCleaner.run(bRegion, colors);
+	//
+	// // create new board image
+	//
+	// BoardRegion bimg = new BoardRegion();
+	//
+	// bimg.setIcon(Util.resize(binarized, ICON_SCALE));
+	// bimg.setPixels(binarized);
+	// bimg.setSource(this);
+	// bimg.setSrcQuadrangle(quadrangle);
+	//
+	// if(colors[1] == Color.BLACK){
+	// bimg.setType(BoardRegion.BoardType.BLACK);
+	// } else if(colors[2] == Color.WHITE){
+	// bimg.setType(BoardRegion.BoardType.WHITE);
+	// }
+	//
+	// return bimg;
+	// }
+
 	@Override
 	public String toString() {
 		return "SourceImage [id=" + id + ", name=" + name + ", date=" + date
